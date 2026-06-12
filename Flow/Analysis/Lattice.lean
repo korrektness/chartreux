5c900e0c61b0dc33cd675755565716dda3e148a7
shilangyu <xmarcinmarcin@gmail.com>
2026-06-12 17:14:00 +0200

    feat: add product and optional lattice

--- Flow/Analysis/Lattice.lean
+++ Flow/Analysis/Lattice.lean
@@ -140,4 +140,76 @@ instance {n : Nat} [FiniteHeight A]
   bot_le a := by funext i; exact ll.bot_le (a i)
 
 end Domain
+
+-- A pointwise product lattice L₁ × L₂
+section Product
+
+variable {L₁ L₂ : Type} [Max L₁] [Max L₂] [Bot L₁] [Bot L₂]
+variable [fh₁ : FiniteHeight L₁] [fh₂ : FiniteHeight L₂]
+variable [ll₁ : LatticeLike L₁] [ll₂ : LatticeLike L₂]
+
+instance : Max (L₁ × L₂) where
+  max
+  | (a₁, b₁), (a₂, b₂) => (max a₁ a₂, max b₁ b₂)
+
+instance : Bot (L₁ × L₂) where
+  bot := (⊥, ⊥)
+
+instance : FiniteHeight (L₁ × L₂) where
+  remainingHeight
+  | (a, b) => FiniteHeight.remainingHeight a + FiniteHeight.remainingHeight b
+  height_join := by
+    intro ⟨a₁, b₁⟩ ⟨a₂, b₂⟩ hmax
+    simp only [max, ne_eq, Prod.mk.injEq, not_and] at *
+    have ne : a₁ ⊔ a₂ ≠ a₁ ∨ b₁ ⊔ b₂ ≠ b₁ := by grind
+    cases ne with
+    | inl h => grind [fh₁.height_join _ _ h, fh₂.height_le_of_join b₁ b₂]
+    | inr h => grind [fh₂.height_join _ _ h, fh₁.height_le_of_join a₁ a₂]
+
+instance : LatticeLike (L₁ × L₂) where
+  join_comm := by simp [max]; grind [ll₁.join_comm, ll₂.join_comm]
+  join_assoc := by simp [max]; grind [ll₁.join_assoc, ll₂.join_assoc]
+  join_idem := by simp [max]; grind [ll₁.join_idem, ll₂.join_idem]
+  bot_le := by simp [max]; grind [ll₁.bot_le, ll₂.bot_le]
+
+end Product
+
+-- A optional lattice `Option L`
+section Option
+
+variable {L : Type} [Max L] [Bot L]
+variable [fh : FiniteHeight L] [ll : LatticeLike L]
+
+instance : Max (Option L) where
+  max
+  | a, .none => a
+  | .none, b => b
+  | .some a, .some b => .some (max a b)
+
+instance : Bot (Option L) where
+  bot := none
+
+instance : FiniteHeight (Option L) where
+  remainingHeight
+  | .none => FiniteHeight.remainingHeight ⊥ + 1
+  | .some a => FiniteHeight.remainingHeight a
+  height_join := by
+    intro a b hmax
+    cases a <;> cases b <;>
+      simp only [max, ne_eq, not_true_eq_false, Option.some.injEq] at * <;> expose_names
+    · have h_bot : ⊥ ⊑ val := ll.bot_le val
+      have h_le := fh.height_le_of_join ⊥ val
+      rw [h_bot] at h_le
+      omega
+    · apply fh.height_join
+      assumption
+
+instance : LatticeLike (Option L) where
+  join_comm := by simp [max]; grind [ll.join_comm]
+  join_assoc := by simp [max]; grind [ll.join_assoc]
+  join_idem := by simp [max]; grind [ll.join_idem]
+  bot_le := by intro a; cases a <;> simp [max]; rfl
+
+end Option
+
 end Basics
